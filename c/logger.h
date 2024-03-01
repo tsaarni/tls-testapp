16464dcd933d9a20ed5decc619153faaa91acca6
Tero Saarni <tero.saarni@est.tech>
2024-03-01 17:54:36 +0200

    c/readme file not needed

---
+++ c/logger.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <openssl/err.h>
+#include <time.h>
+
+#define DEBUG_SET_THREAD_NAME(name) thread_name = name
+
+#define DEBUG(fmt, ...) LOGGER(fmt, "DEBUG", ##__VA_ARGS__)
+#define PERROR(fmt, ...)                                                       \
+    do {                                                                       \
+        unsigned long openssl_err = ERR_get_error();                           \
+        if (errno)                                                             \
+            LOGGER(fmt ": %s", "ERROR", ##__VA_ARGS__, strerror(errno));    \
+        else if (openssl_err) {                                                \
+            char buf[256];                                                     \
+            ERR_error_string_n(openssl_err, buf, sizeof(buf));                 \
+            LOGGER(fmt ": %s", "ERROR", ##__VA_ARGS__, buf);                \
+        } else                                                                 \
+            LOGGER(fmt, "ERROR", ##__VA_ARGS__);                            \
+    } while (0)
+
+#define LOGGER(fmt, level, ...)                                             \
+    do {                                                                       \
+        time_t t = time(NULL);                                                 \
+        struct tm tm = *localtime(&t);                                         \
+        fprintf(stderr,                                                        \
+                "%d-%02d-%02dT%02d:%02d:%02d [%s] %s:%d " level ": " fmt "\n", \
+                tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,      \
+                tm.tm_min, tm.tm_sec, thread_name, __FILE__, __LINE__,         \
+                ##__VA_ARGS__);                                                \
+    } while (0)
--- c/testapp.c
+++ c/testapp.c
@@ -13,282 +13,289 @@
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
 
-#include "debug.h"
+#include "logger.h"
 
 #define SERVER_ADDR "localhost"
 #define SERVER_PORT 9876
 
 #define KEYLOG_FILE "wireshark-keylog.txt"
 
+// Allocate a thread-local variable for the thread name.
+_Thread_local char *thread_name = "unknown";
+
 int create_socket(char *hostname, int port, struct addrinfo **res) {
-	DEBUG("Creating socket");
+    DEBUG("Creating socket");
 
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		PERROR("socket");
-		exit(EXIT_FAILURE);
-	}
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        PERROR("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	struct addrinfo hints;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
-	char port_str[6];
-	snprintf(port_str, sizeof(port_str), "%d", port);
+    char port_str[6];
+    snprintf(port_str, sizeof(port_str), "%d", port);
 
-	if (getaddrinfo(hostname, port_str, &hints, res) != 0) {
-		PERROR("getaddrinfo");
-		exit(EXIT_FAILURE);
-	}
+    if (getaddrinfo(hostname, port_str, &hints, res) != 0) {
+        PERROR("getaddrinfo");
+        exit(EXIT_FAILURE);
+    }
 
-	return sock;
+    return sock;
 }
 
 int load_credentials(SSL_CTX *ctx, char *cert_file, char *key_file) {
-	if (SSL_CTX_use_certificate_file(ctx, cert_file, SSL_FILETYPE_PEM) <= 0) {
-		PERROR("SSL_CTX_use_certificate_file");
-		return -1;
-	}
+    if (SSL_CTX_use_certificate_file(ctx, cert_file, SSL_FILETYPE_PEM) <= 0) {
+        PERROR("SSL_CTX_use_certificate_file");
+        return -1;
+    }
 
-	if (SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM) <= 0) {
-		PERROR("SSL_CTX_use_PrivateKey_file");
-		return -1;
-	}
+    if (SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM) <= 0) {
+        PERROR("SSL_CTX_use_PrivateKey_file");
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 int set_min_max_proto_versions(SSL_CTX *ctx, int min_version, int max_version) {
-	if (SSL_CTX_set_min_proto_version(ctx, min_version) != 1) {
-		PERROR("SSL_CTX_set_min_proto_version");
-		return -1;
-	}
+    if (SSL_CTX_set_min_proto_version(ctx, min_version) != 1) {
+        PERROR("SSL_CTX_set_min_proto_version");
+        return -1;
+    }
 
-	if (SSL_CTX_set_max_proto_version(ctx, max_version) != 1) {
-		PERROR("SSL_CTX_set_max_proto_version");
-		return -1;
-	}
+    if (SSL_CTX_set_max_proto_version(ctx, max_version) != 1) {
+        PERROR("SSL_CTX_set_max_proto_version");
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 void keylog_callback(const SSL *ssl, const char *line) {
-	(void)ssl;
+    (void)ssl;
 
-	FILE *file = fopen(KEYLOG_FILE, "a");
-	if (!file) {
-		PERROR("fopen");
-		return;
-	}
+    FILE *file = fopen(KEYLOG_FILE, "a");
+    if (!file) {
+        PERROR("fopen");
+        return;
+    }
 
-	fprintf(file, "%s\n", line);
-	fclose(file);
+    fprintf(file, "%s\n", line);
+    fclose(file);
 }
 
 void print_peer_cert_info(SSL *ssl) {
-	X509 *cert = SSL_get_peer_certificate(ssl);
-	X509_NAME *name = X509_get_subject_name(cert);
+    X509 *cert = SSL_get_peer_certificate(ssl);
+    X509_NAME *name = X509_get_subject_name(cert);
 
-	BIO *bio = BIO_new(BIO_s_mem());
-	X509_NAME_print_ex(bio, name, 0, XN_FLAG_RFC2253);
+    BIO *bio = BIO_new(BIO_s_mem());
+    X509_NAME_print_ex(bio, name, 0, XN_FLAG_RFC2253);
 
-	char *subject = NULL;
-	BIO_get_mem_data(bio, &subject);
-	DEBUG("Subject: %s", subject);
+    char *subject = NULL;
+    BIO_get_mem_data(bio, &subject);
+    DEBUG("Subject: %s", subject);
 
-	BIO_free(bio);
-	X509_free(cert);
+    BIO_free_all(bio);
+    X509_free(cert);
 }
 
 void *run_client(void *arg) {
-	(void)arg;
+    (void)arg;
 
-	DEBUG_SET_THREAD_NAME("client");
+    DEBUG_SET_THREAD_NAME("client");
 
-	SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-	if (!ctx) {
-		perror("SSL_CTX_new");
-		return NULL;
-	}
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        PERROR("SSL_CTX_new");
+        return NULL;
+    }
 
-	SSL_CTX_set_keylog_callback(ctx, keylog_callback);
+    SSL_CTX_set_keylog_callback(ctx, keylog_callback);
 
-	if (load_credentials(ctx, "client.pem", "client-key.pem") != 0) {
-		return NULL;
-	}
+    if (load_credentials(ctx, "client.pem", "client-key.pem") != 0) {
+        return NULL;
+    }
 
-	if (!SSL_CTX_load_verify_locations(ctx, "server-ca.pem", NULL)) {
-		PERROR("SSL_CTX_load_verify_locations");
-		return NULL;
-	}
+    if (!SSL_CTX_load_verify_locations(ctx, "server-ca.pem", NULL)) {
+        PERROR("SSL_CTX_load_verify_locations");
+        return NULL;
+    }
 
-	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
 
-	DEBUG("Connecting to %s:%d", SERVER_ADDR, SERVER_PORT);
+    DEBUG("Connecting to %s:%d", SERVER_ADDR, SERVER_PORT);
 
-	struct addrinfo *res;
-	int sock = create_socket(SERVER_ADDR, SERVER_PORT, &res);
+    struct addrinfo *res;
+    int sock = create_socket(SERVER_ADDR, SERVER_PORT, &res);
 
-	// Retry connection until success
-	while (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
-		PERROR("Attempting reconnect...");
-		usleep(100000);
-	}
+    // Retry connection until success
+    while (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
+        PERROR("Attempting reconnect...");
+        usleep(100000);
+    }
+    freeaddrinfo(res);
 
-	SSL *ssl = SSL_new(ctx);
-	if (!ssl) {
-		PERROR("SSL_new");
-		return NULL;
-	}
-	SSL_set_fd(ssl, sock);
+    SSL *ssl = SSL_new(ctx);
+    if (!ssl) {
+        PERROR("SSL_new");
+        return NULL;
+    }
+    SSL_set_fd(ssl, sock);
 
-	DEBUG("SSL_connect");
-	if (SSL_connect(ssl) <= 0) {
-		PERROR("SSL_connect");
-		return NULL;
-	}
+    DEBUG("SSL_connect");
+    if (SSL_connect(ssl) <= 0) {
+        PERROR("SSL_connect");
+        return NULL;
+    }
 
-	DEBUG("Connected with TLS version %s, cipher %s", SSL_get_version(ssl),
-		  SSL_get_cipher(ssl));
+    DEBUG("Connected with TLS version %s, cipher %s", SSL_get_version(ssl),
+          SSL_get_cipher(ssl));
 
-	print_peer_cert_info(ssl);
+    print_peer_cert_info(ssl);
 
-	if (SSL_get_verify_result(ssl) != X509_V_OK) {
-		PERROR("SSL_get_verify_result");
-		return NULL;
-	}
+    if (SSL_get_verify_result(ssl) != X509_V_OK) {
+        PERROR("SSL_get_verify_result");
+        return NULL;
+    }
 
-	X509 *cert = SSL_get_peer_certificate(ssl);
-	if (!cert) {
-		PERROR("SSL_get_peer_certificate");
-		return NULL;
-	}
+    X509 *cert = SSL_get_peer_certificate(ssl);
+    if (!cert) {
+        PERROR("SSL_get_peer_certificate");
+        return NULL;
+    }
 
-	if (X509_check_host(cert, SERVER_ADDR, 0, 0, NULL) != 1) {
-		DEBUG("Certificate does not match the server hostname");
-		return NULL;
-	}
+    if (X509_check_host(cert, SERVER_ADDR, 0, 0, NULL) != 1) {
+        DEBUG("Certificate does not match the server hostname");
+        return NULL;
+    }
 
-	DEBUG("Sending: Hello, world!");
-	int len = SSL_write(ssl, "Hello, world!", 13);
-	if (len < 0) {
-		PERROR("SSL_write");
-		return NULL;
-	}
+    DEBUG("Sending: Hello, world!");
+    int len = SSL_write(ssl, "Hello, world!", 13);
+    if (len < 0) {
+        PERROR("SSL_write");
+        return NULL;
+    }
 
-	// TLSv1.3: we need to read (at least) 0 bytes to complete the handshake.
-	len = SSL_read(ssl, NULL, 0);
+    // TLSv1.3: we need to read (at least) 0 bytes to complete the handshake.
+    len = SSL_read(ssl, NULL, 0);
 
-	SSL_free(ssl);
-	close(sock);
-	SSL_CTX_free(ctx);
+    SSL_free(ssl);
+    close(sock);
+    SSL_CTX_free(ctx);
 
-	return 0;
+    return 0;
 }
 
 void *run_server(void *arg) {
-	(void)arg;
+    (void)arg;
 
-	DEBUG_SET_THREAD_NAME("server");
+    DEBUG_SET_THREAD_NAME("server");
 
-	SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
-	if (!ctx) {
-		PERROR("SSL_CTX_new");
-		return NULL;
-	}
+    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
+        PERROR("SSL_CTX_new");
+        return NULL;
+    }
 
-	SSL_CTX_set_keylog_callback(ctx, keylog_callback);
+    SSL_CTX_set_keylog_callback(ctx, keylog_callback);
 
-	if (set_min_max_proto_versions(ctx, TLS1_2_VERSION, TLS1_3_VERSION) != 0) {
-		return NULL;
-	}
+    if (set_min_max_proto_versions(ctx, TLS1_2_VERSION, TLS1_3_VERSION) != 0) {
+        return NULL;
+    }
 
-	if (load_credentials(ctx, "server.pem", "server-key.pem") != 0) {
-		return NULL;
-	}
+    if (load_credentials(ctx, "server.pem", "server-key.pem") != 0) {
+        return NULL;
+    }
 
-	if (!SSL_CTX_load_verify_locations(ctx, "client-ca.pem", NULL)) {
-		PERROR("SSL_CTX_load_verify_locations");
-		exit(EXIT_FAILURE);
-	}
+    if (!SSL_CTX_load_verify_locations(ctx, "client-ca.pem", NULL)) {
+        PERROR("SSL_CTX_load_verify_locations");
+        exit(EXIT_FAILURE);
+    }
 
-	SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT,
-					   NULL);
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT,
+                       NULL);
 
-	struct addrinfo *res;
-	int sock = create_socket(SERVER_ADDR, SERVER_PORT, &res);
-	if (bind(sock, res->ai_addr, res->ai_addrlen) != 0) {
-		PERROR("bind");
-		return NULL;
-	}
+    struct addrinfo *res;
+    int sock = create_socket(SERVER_ADDR, SERVER_PORT, &res);
+    if (bind(sock, res->ai_addr, res->ai_addrlen) != 0) {
+        PERROR("bind");
+        return NULL;
+    }
+    freeaddrinfo(res);
 
-	DEBUG("Listening on %s:%d", SERVER_ADDR, SERVER_PORT);
-	if (listen(sock, 1) != 0) {
-		PERROR("listen");
-		return NULL;
-	}
+    DEBUG("Listening on %s:%d", SERVER_ADDR, SERVER_PORT);
+    if (listen(sock, 1) != 0) {
+        PERROR("listen");
+        return NULL;
+    }
 
-	struct sockaddr_in addr;
-	socklen_t addr_len = sizeof(addr);
-	int client_sock = accept(sock, (struct sockaddr *)&addr, &addr_len);
-	if (client_sock < 0) {
-		perror("accept");
-		return NULL;
-	}
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+    int client_sock = accept(sock, (struct sockaddr *)&addr, &addr_len);
+    if (client_sock < 0) {
+        PERROR("accept");
+        return NULL;
+    }
 
-	DEBUG("Client connected from %s:%d", inet_ntoa(addr.sin_addr),
-		  ntohs(addr.sin_port));
+    DEBUG("Client connected from %s:%d", inet_ntoa(addr.sin_addr),
+          ntohs(addr.sin_port));
 
-	SSL *ssl = SSL_new(ctx);
-	if (!ssl) {
-		PERROR("SSL_new");
-		return NULL;
-	}
-	SSL_set_fd(ssl, client_sock);
+    SSL *ssl = SSL_new(ctx);
+    if (!ssl) {
+        PERROR("SSL_new");
+        return NULL;
+    }
+    SSL_set_fd(ssl, client_sock);
 
-	DEBUG("SSL_accept");
-	if (SSL_accept(ssl) <= 0) {
-		PERROR("SSL_accept");
-		return NULL;
-	}
+    DEBUG("SSL_accept");
+    if (SSL_accept(ssl) <= 0) {
+        PERROR("SSL_accept");
+        return NULL;
+    }
 
-	print_peer_cert_info(ssl);
+    print_peer_cert_info(ssl);
 
-	if (SSL_get_verify_result(ssl) != X509_V_OK) {
-		PERROR("SSL_get_verify_result");
-		return NULL;
-	}
+    if (SSL_get_verify_result(ssl) != X509_V_OK) {
+        PERROR("SSL_get_verify_result");
+        return NULL;
+    }
 
-	DEBUG("Connected with TLS version %s, cipher %s", SSL_get_version(ssl),
-		  SSL_get_cipher(ssl));
+    DEBUG("Connected with TLS version %s, cipher %s", SSL_get_version(ssl),
+          SSL_get_cipher(ssl));
 
-	char buf[1024];
-	int len = SSL_read(ssl, buf, sizeof(buf) - 1);
-	if (len < 0) {
-		PERROR("SSL_read");
-		return NULL;
-	}
-	buf[len] = '\0';
-	DEBUG("Received: %s", buf);
+    char buf[1024];
+    int len = SSL_read(ssl, buf, sizeof(buf) - 1);
+    if (len < 0) {
+        PERROR("SSL_read");
+        return NULL;
+    }
+    buf[len] = '\0';
+    DEBUG("Received: %s", buf);
 
-	SSL_free(ssl);
-	close(client_sock);
-	close(sock);
-	SSL_CTX_free(ctx);
+    SSL_free(ssl);
+    close(client_sock);
+    close(sock);
+    SSL_CTX_free(ctx);
 
-	return 0;
+    return 0;
 }
 
 int main() {
-	pthread_t server_thread;
-	pthread_create(&server_thread, NULL, run_server, NULL);
+    pthread_t server_thread;
+    pthread_create(&server_thread, NULL, run_server, NULL);
 
-	pthread_t client_thread;
-	pthread_create(&client_thread, NULL, run_client, NULL);
+    pthread_t client_thread;
+    pthread_create(&client_thread, NULL, run_client, NULL);
 
-	pthread_join(client_thread, NULL);
-	pthread_join(server_thread, NULL);
+    pthread_join(client_thread, NULL);
+    pthread_join(server_thread, NULL);
 
-	return 0;
+	DEBUG("Server and client threads have finished. Exiting.");
+
+    return 0;
 }
